898bc5294d518a3270bd6c6bb610531f341aec35
hithedy <ghhedy1987>
2017-07-28 21:32:37 +0800

    飞行日志模块demo

--- src/main/java/com/xyzq/kid/console/action/RecordAction.java
+++ src/main/java/com/xyzq/kid/console/action/RecordAction.java
@@ -1,6 +1,6 @@
 package com.xyzq.kid.console.action;
 
-import com.xyzq.kid.logic.record.service.FlyRecordService;
+import com.xyzq.kid.logic.record.service.RecordService;
 import com.xyzq.simpson.base.json.JSONObject;
 import com.xyzq.simpson.maggie.access.spring.MaggieAction;
 import com.xyzq.simpson.maggie.framework.Context;
@@ -8,18 +8,17 @@ import com.xyzq.simpson.maggie.framework.Visitor;
 import com.xyzq.simpson.maggie.framework.action.core.IAction;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import javax.annotation.Resource;
 
 /**
  * 范例动作
  */
-@MaggieAction(path = "kid/console/flyRecord")
-public class FlyRecordAction implements IAction {
+@MaggieAction(path = "kid/console/record")
+public class RecordAction implements IAction {
     /**
      * Action中只支持Autowired注解引入SpringBean
      */
     @Autowired
-    private FlyRecordService flyRecordService;
+    private RecordService recordService;
 
 
     /**
@@ -32,7 +31,7 @@ public class FlyRecordAction implements IAction {
     @Override
     public String execute(Visitor visitor, Context context) throws Exception {
         context.set("msg", "这个是前端需要展示的消息");
-        context.set("data", JSONObject.convertFromObject(flyRecordService.call()).toString());
+        context.set("data", JSONObject.convertFromObject(recordService.call()).toString());
         return "success.json";
     }
 
