7691c24e4e783cb7b2312274085b065bf50e6373
sovranliu <sovranliu@aliyun.com>
2017-08-05 18:07:49 +0800

    ......

--- src/main/java/com/xyzq/kid/console/action/pay/OrderQueryAction.java
+++ src/main/java/com/xyzq/kid/console/action/pay/OrderQueryAction.java
@@ -63,8 +63,8 @@ public class OrderQueryAction implements IAction {
             endTime = DateTime.parse((String) context.parameter("endTime"));
         }
         catch (Exception ex) { }
-        int begin = (Integer) context.parameter("status", 0);
-        int size = (Integer) context.parameter("status", 10);
+        int begin = (Integer) context.parameter("begin", 0);
+        int size = (Integer) context.parameter("size", 10);
         List<OrderInfoEntity> orderInfoEntityList = orderService.find(orderNo, openId, status, beginTime, endTime, begin, size);
         context.set("data", JSONArray.convertFromSet(orderInfoEntityList));
         return "success.json";
--- src/main/java/com/xyzq/kid/console/action/pay/RefundQueryAction.java
+++ src/main/java/com/xyzq/kid/console/action/pay/RefundQueryAction.java
@@ -63,8 +63,8 @@ public class RefundQueryAction implements IAction {
             endTime = DateTime.parse((String) context.parameter("endTime"));
         }
         catch (Exception ex) { }
-        int begin = (Integer) context.parameter("status", 0);
-        int size = (Integer) context.parameter("status", 10);
+        int begin = (Integer) context.parameter("begin", 0);
+        int size = (Integer) context.parameter("size", 10);
         List<RefundInfoEntity> refundInfoEntityList = refundService.find(orderNo, openId, status, beginTime, endTime, begin, size);
         context.set("data", JSONArray.convertFromSet(refundInfoEntityList));
         return "success.json";
