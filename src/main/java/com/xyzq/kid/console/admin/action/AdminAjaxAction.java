f26e9762642751993f71f1fef81248bc7311f528
sovranliu <sovranliu@aliyun.com>
2017-08-05 19:21:44 +0800

    新增登录管理员基类动作

---
+++ src/main/java/com/xyzq/kid/console/admin/action/AdminAjaxAction.java
@@ -0,0 +1,69 @@
+package com.xyzq.kid.console.admin.action;
+
+import com.xyzq.kid.logic.admin.service.AdminService;
+import com.xyzq.simpson.base.text.Text;
+import com.xyzq.simpson.maggie.framework.Context;
+import com.xyzq.simpson.maggie.framework.Visitor;
+import com.xyzq.simpson.maggie.framework.action.core.IAction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+/**
+ * 登录管理员基类
+ */
+public abstract class AdminAjaxAction implements IAction {
+    /**
+     * 上下文中的键
+     */
+    public final static String CONTEXT_KEY_AID = "aid";
+
+    /**
+     * 站点域名
+     */
+    @Value("${KID.CONSOLE.URL_DOMAIN}")
+    public String url_domain;
+    @Value("${KID.CONSOLE.URL_PAGE_LOGIN}")
+    public String url_page_login;
+    /**
+     * 管理员服务
+     */
+    @Autowired
+    protected AdminService adminService;
+    /**
+     * 管理员ID
+     */
+    protected int adminId = 0;
+
+
+    /**
+     * 动作执行
+     *
+     * @param visitor 访问者
+     * @param context 请求上下文
+     * @return 下一步动作，包括后缀名，null表示结束
+     */
+    @Override
+    public String execute(Visitor visitor, Context context) throws Exception {
+        String aId = visitor.cookie("aid");
+        if(!Text.isBlank(aId)) {
+            Integer id = adminService.fetchAdminId(aId);
+            if(null != id) {
+                adminId = id;
+            }
+        }
+        if(0 == adminId) {
+            context.put("redirect", url_page_login);
+            return "fail.json";
+        }
+        return doExecute(visitor, context);
+    }
+
+    /**
+     * 派生类动作执行
+     *
+     * @param visitor 访问者
+     * @param context 请求上下文
+     * @return 下一步动作，包括后缀名，null表示结束
+     */
+    public abstract String doExecute(Visitor visitor, Context context) throws Exception;
+}
