b7ca79ec35dc3b68d290bc6447ed167d5cdb84f4
keyanggui <273927457@qq.com>
2017-08-12 15:12:23 +0800

    quartz

---
+++ src/main/java/com/xyzq/kid/console/book/action/GetBookingTime.java
@@ -0,0 +1,61 @@
+package com.xyzq.kid.console.book.action;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.google.gson.Gson;
+import com.mysql.jdbc.StringUtils;
+import com.xyzq.kid.logic.book.dao.po.BookTimeRepository;
+import com.xyzq.kid.logic.book.dao.po.BookTimeSpan;
+import com.xyzq.kid.logic.book.service.BookRepositoryService;
+import com.xyzq.kid.logic.book.service.BookTimeSpanService;
+import com.xyzq.simpson.maggie.access.spring.MaggieAction;
+import com.xyzq.simpson.maggie.framework.Context;
+import com.xyzq.simpson.maggie.framework.Visitor;
+import com.xyzq.simpson.maggie.framework.action.core.IAction;
+
+@MaggieAction(path="kid/console/getBookingTime")
+public class GetBookingTime implements IAction {
+	
+	@Autowired
+	BookRepositoryService bookRepositoryService;
+	@Autowired
+	BookTimeSpanService bookTimeSpanService;
+	
+	Gson gson=new Gson();
+	
+	@Override
+	public String execute(Visitor visitor, Context context) throws Exception {
+		List<Map<String,String>> spanList=new ArrayList<>();
+		String year=(String)context.parameter("year");
+		String month=(String)context.parameter("month");
+		String day=(String)context.parameter("day");
+		if(!StringUtils.isNullOrEmpty(year)&&!StringUtils.isNullOrEmpty(month)&&!StringUtils.isNullOrEmpty(day)){
+			String bookDate=year+"-"+month+"-"+day;
+			List<BookTimeRepository> repos=bookRepositoryService.queryRepositoryByDate(bookDate);
+			if(repos!=null&&repos.size()>0){
+				for(BookTimeRepository repo:repos){
+					if(repo.getBooktotal()-repo.getBookamount()>0){
+						Integer spanTimeId=repo.getBooktimespanid();
+						BookTimeSpan bs=bookTimeSpanService.queryByPrimaryKey(spanTimeId);
+						if(bs.getTimespan()!=null&&bs.getTimespan().contains("-")){
+							Map<String,String> map=new HashMap<>();
+							String[] times=bs.getTimespan().split("-");
+							map.put("start", times[0]);
+							map.put("end", times[1]);
+							spanList.add(map);
+						}
+					}
+				}
+			}
+		}
+		context.set("code", 0);
+		context.set("data", gson.toJson(spanList));
+		return "success.json";
+	}
+
+}
--- src/main/java/com/xyzq/kid/console/job/InitBookingRepositoryJob.java
+++ src/main/java/com/xyzq/kid/console/job/InitBookingRepositoryJob.java
@@ -1,47 +1,97 @@
 package com.xyzq.kid.console.job;
 
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.quartz.QuartzJobBean;
 
 import com.mysql.jdbc.StringUtils;
 import com.xyzq.kid.logic.book.service.BookRepositoryService;
 import com.xyzq.kid.logic.config.service.ConfigService;
+import com.xyzq.kid.logic.dateUnviable.entity.DateUnviableEntity;
+import com.xyzq.kid.logic.dateUnviable.service.DateUnviableService;
 
-public class InitBookingRepositoryJob extends QuartzJobBean{
+public class InitBookingRepositoryJob{
 	
+	public static Logger logger = LoggerFactory.getLogger(InitBookingRepositoryJob.class);
 
 	@Autowired
 	BookRepositoryService bookRepositoryService;
+	
+	@Autowired
+	DateUnviableService dateUnviableService;
 	
 	@Autowired
 	ConfigService configService;
 	
 	static final String BOOK_REPOSITORY_COUNT="book_repository_count";
 	static final String BOOK_MAX_DAYS="book_max_days";
+	static final String REPOSITORY_LASTINIT_DATE="repository_lastinit_date";
+	static final String IP_WHITE_NAMELIST="ip_white_namelist";//IP白名单，名单内的服务器可以执行该定时任务
+	
+	SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
 
-	@Override
-	protected void executeInternal(JobExecutionContext context)
-			throws JobExecutionException {
-		String repoCount=configService.fetch(BOOK_REPOSITORY_COUNT);
-		
-		String maxBookDays=configService.fetch(BOOK_MAX_DAYS);
-		
-		String bookDate=null;
-		
-		if(StringUtils.isNullOrEmpty(repoCount)){
-			repoCount="5";
+	protected void init() {
+		String ips=configService.fetch(IP_WHITE_NAMELIST);
+		try {
+			if(StringUtils.isNullOrEmpty(ips)){
+				work();
+			}else{
+				String localIP=InetAddress.getLocalHost().getHostAddress();
+				if(ips.contains(localIP)){
+					work();
+				}
+			}
+		} catch (UnknownHostException e1) {
+			e1.printStackTrace();
 		}
-		
-		if(StringUtils.isNullOrEmpty(maxBookDays)){
-			maxBookDays="180";
+	}
+	
+	private void work() {
+		String repoCount = configService.fetch(BOOK_REPOSITORY_COUNT);
+
+		String maxBookDays = configService.fetch(BOOK_MAX_DAYS);
+
+		String bookDate = configService.fetch(REPOSITORY_LASTINIT_DATE);
+
+		if (StringUtils.isNullOrEmpty(repoCount)) {
+			repoCount = "5";
 		}
-		
-		if(bookRepositoryService.initRepositoryByDate(bookDate, Integer.valueOf(repoCount))){
-			
+
+		if (StringUtils.isNullOrEmpty(maxBookDays)) {
+			maxBookDays = "180";
 		}
-		
+		try {
+			Date lastDate = sdf.parse(bookDate);
+			Calendar last = Calendar.getInstance();
+			last.setTime(lastDate);
+			Date nowDate=new Date();
+			Calendar now=Calendar.getInstance();
+			now.setTime(nowDate);
+			Calendar max = Calendar.getInstance();
+			max.setTime(nowDate);
+			max.add(Calendar.DATE, Integer.parseInt(maxBookDays));
+			while (last.before(max)) {
+				last.add(Calendar.DATE, 1);
+				String initDate = sdf.format(last.getTime());
+				DateUnviableEntity unviableDate = dateUnviableService
+						.findBy(initDate);
+				if (unviableDate == null) {
+					if (bookRepositoryService.initRepositoryByDate(initDate,
+							Integer.valueOf(repoCount))) {
+						configService.alter(REPOSITORY_LASTINIT_DATE, initDate);
+						logger.info(initDate + " repository init success");
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
